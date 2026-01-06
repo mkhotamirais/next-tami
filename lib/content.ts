079179c553a49b1a1390a70df8e7167a07da84c9
mkhotamirais <mkhotamirais@gmail.com>
2026-01-06 20:13:38 +0700

    selesai

--- lib/content.ts
+++ lib/content.ts
@@ -32,6 +32,10 @@ export const mainMenu = [
     url: "/other",
     subMenu: [
       { label: "Sticky", url: "/other/sticky" },
+      { label: "Clock", url: "/other/clock" },
+      { label: "Stopwatch", url: "/other/stopwatch" },
+      { label: "Symbol Color", url: "/other/symbol-color" },
+      { label: "Ts1", url: "/other/ts1" },
       { label: "Ts2", url: "/other/ts2" },
       { label: "Ts3", url: "/other/ts3" },
     ],
